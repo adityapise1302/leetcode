f5420fb0b83396a6e6e7ec02cc52722b41334219
Aditya Pise <73094690+adityapise1302@users.noreply.github.com>
2025-12-22 19:40:27 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (93.94%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,34 +1,15 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        if not matrix or not matrix[0]:
-            return False
-
-        l_m, r_m = 0, len(matrix) - 1
-        row = -1
-        while l_m <= r_m:
-            mid_m = (l_m + r_m) // 2
-            first = matrix[mid_m][0]
-            last  = matrix[mid_m][-1]
-            if first <= target <= last:
-                row = mid_m
-                break
-            elif target < first:
-                r_m = mid_m - 1
-            else: 
-                l_m = mid_m + 1
-
-        if row == -1:
-            return False
-
-        l_n, r_n = 0, len(matrix[row]) - 1
-        while l_n <= r_n:
-            mid_n = (l_n + r_n) // 2
-            val = matrix[row][mid_n]
-            if val == target:
+        l, r = 0, (len(matrix) * len(matrix[0])) - 1
+        n = len(matrix[0])
+        while l <= r:
+            mid = (l + r) // 2
+            row = mid // n
+            col = mid % n
+            if matrix[row][col] == target:
                 return True
-            elif val < target:
-                l_n = mid_n + 1
+            elif matrix[row][col] > target:
+                r = mid - 1
             else:
-                r_n = mid_n - 1
-
+                l = mid + 1
         return False
