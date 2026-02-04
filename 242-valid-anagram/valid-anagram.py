7d02fff8714769d7b2ee019e8cb2c4ba808d02f7
Aditya Pise <73094690+adityapise1302@users.noreply.github.com>
2026-02-04 10:09:47 -0700

    Time: 8 ms (81.17%) | Memory: 19.4 MB (48.38%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,13 +1,16 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        char_arr = [0]*26
+        hashTable = dict()
         if len(s) != len(t):
             return False
         for char in s:
-            char_arr[ord(char) - ord("a")] += 1
+            hashTable[char] = hashTable.get(char, 0) + 1
         for char in t:
-            if char_arr[ord(char) - ord("a")] > 0:
-                char_arr[ord(char) - ord("a")] -= 1
+            if hashTable.get(char, 0) > 0:
+                hashTable[char] -= 1
             else:
+                return False
+        for key, val in hashTable.items():
+            if val != 0:
                 return False
         return True
