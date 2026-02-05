800c77da0c93be3d20e494f8d95478d29c3fa41a
Aditya Pise <73094690+adityapise1302@users.noreply.github.com>
2026-02-04 22:27:43 -0700

    Time: 14 ms (12.18%) | Memory: 24.9 MB (7.41%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,16 +1,16 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        trackHash = dict()
-        resKFreq = []
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+        res = []
         for num in nums:
-            trackHash[num] = trackHash.get(num, 0) + 1
-        for i in range(k):
-            maxFreq = 0
-            n = -1
-            for num, freq in trackHash.items():
-                if freq > maxFreq:
-                    n = num
-                    maxFreq = freq
-            resKFreq.append(n)
-            trackHash[n] = 0
-        return resKFreq
+            count[num] = count.get(num, 0) + 1
+        for n, c in count.items():
+            freq[c].append(n)
+        idx = len(freq) - 1
+        while k > 0:
+            res += freq[idx]
+            k -= len(freq[idx])
+            idx -= 1
+        return res
+            
