c2dddda84d8c91afa769861b89a4b2505c37d916
Aditya Pise <73094690+adityapise1302@users.noreply.github.com>
2026-04-04 20:03:10 -0700

    Time: 0 ms (100.00%) | Memory: 19.4 MB (98.51%) - LeetSync

--- 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.py
+++ 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.py
@@ -1,6 +1,5 @@
 class Solution:
     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
-        print(boxTypes)
         boxTypes.sort(key=lambda boxType: boxType[1], reverse = True)
         res, currBox = 0, 0
         while truckSize > 0 and currBox < len(boxTypes):
